9d3f8a08fd22905c3dce2a3a279a7362a7960851
Ilya Mozerov <imozerov@mera.ru>
2016-02-12 11:59:49 +0300

    Fixes #21 Moved DataSource to the presenter class. Now fragment which is view has no refs to DataSource.

--- app/src/main/java/org/imozerov/streetartview/ui/explore/list/ArtListFragment.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/explore/list/ArtListFragment.kt
@@ -3,19 +3,16 @@ package org.imozerov.streetartview.ui.explore.list
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_art_list.view.*
 import org.imozerov.streetartview.R
 import org.imozerov.streetartview.StreetArtViewApp
-import org.imozerov.streetartview.storage.DataSource
 import org.imozerov.streetartview.ui.explore.ArtListPresenter
 import org.imozerov.streetartview.ui.explore.interfaces.ArtView
 import org.imozerov.streetartview.ui.explore.interfaces.Filterable
 import org.imozerov.streetartview.ui.model.ArtObjectUi
-import javax.inject.Inject
 
 class ArtListFragment : Fragment(), Filterable, ArtView {
     private val TAG = "ArtListFragment"
@@ -23,41 +20,35 @@ class ArtListFragment : Fragment(), Filterable, ArtView {
     private var presenter: ArtListPresenter? = null
     private var adapter: ArtListAdapter? = null
 
-    @Inject lateinit var dataSource: DataSource
-
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val rootView = inflater!!.inflate(R.layout.fragment_art_list, container, false)
-        val context = context
 
-        adapter = ArtListAdapter(getContext(), java.util.ArrayList<ArtObjectUi>())
+        presenter = ArtListPresenter(this)
+
+        adapter = ArtListAdapter(context, java.util.ArrayList<ArtObjectUi>())
         rootView.art_objects_recycler_view.setHasFixedSize(true)
         rootView.art_objects_recycler_view.layoutManager = LinearLayoutManager(context)
         rootView.art_objects_recycler_view.adapter = adapter
 
-        // TODO remove this button!
-        rootView.button.setOnClickListener { v ->
-            Log.d(TAG, "adding new ")
-            dataSource.addArtObjectStub()
-        }
+        // TODO remove this button, when server is ready!
+        rootView.button.setOnClickListener { v -> presenter!!.addArtObjectStub()}
 
         return rootView
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        (activity.application as StreetArtViewApp).storageComponent.inject(this)
-    }
-
     override fun onStart() {
         super.onStart()
-        presenter = ArtListPresenter(dataSource, this)
-        presenter!!.onStart()
+        presenter!!.onStart((activity.application as StreetArtViewApp))
     }
 
     override fun onStop() {
         super.onStop()
         presenter!!.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
         presenter = null
     }
 
