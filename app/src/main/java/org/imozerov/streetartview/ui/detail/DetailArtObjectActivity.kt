10f7ccfa248f4ed16e27dcb04d47d2d8ab84d3f4
Ilya Mozerov <imozerov@mera.ru>
2016-02-19 19:00:35 +0300

    Fixes #35 #27
    Added map and gallery to the detail art object view.
    
    Swapped map and list on the main fragment.

--- app/src/main/java/org/imozerov/streetartview/ui/detail/DetailArtObjectActivity.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/detail/DetailArtObjectActivity.kt
@@ -1,12 +1,29 @@
 package org.imozerov.streetartview.ui.detail
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.view.PagerAdapter
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.art_object_gallery_item.view.*
+import kotlinx.android.synthetic.main.fragment_art_map.*
 import org.imozerov.streetartview.R
 import org.imozerov.streetartview.StreetArtViewApp
 import org.imozerov.streetartview.storage.DataSource
+import org.imozerov.streetartview.ui.extensions.addArtObject
+import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 import rx.subscriptions.CompositeSubscription
 import javax.inject.Inject
@@ -15,12 +32,19 @@ import javax.inject.Inject
  * Created by sergei on 08.02.16.
  */
 class DetailArtObjectActivity : AppCompatActivity() {
+    companion object {
+        val EXTRA_KEY_ART_OBJECT_DETAIL_ID = "EXTRA_KEY_ART_OBJECT_DETAIL_ID"
+    }
+
+    val TAG = "DetailArtObjectActivity"
 
     private var compositeSubscription: CompositeSubscription? = null
 
     @Inject
     lateinit var dataSource: DataSource
     private var artObjectId: String? = null
+
+    private var mapFragment: SupportMapFragment? = null
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +53,18 @@ class DetailArtObjectActivity : AppCompatActivity() {
         (application as StreetArtViewApp).appComponent.inject(this)
 
         artObjectId = intent.getStringExtra(EXTRA_KEY_ART_OBJECT_DETAIL_ID)
+
+        mapFragment = SupportMapFragment.newInstance()
+        supportFragmentManager
+                .beginTransaction()
+                .replace(art_object_detail_map.id, mapFragment)
+                .commit()
     }
 
     override fun onStart() {
         super.onStart()
-        this.compositeSubscription = CompositeSubscription()
-        startFetchingArtObject()
+        compositeSubscription = CompositeSubscription()
+        compositeSubscription!!.add(startFetchingArtObject())
     }
 
     public override fun onStop() {
@@ -42,21 +72,65 @@ class DetailArtObjectActivity : AppCompatActivity() {
         compositeSubscription!!.unsubscribe()
     }
 
-    private fun startFetchingArtObject() {
-        this.compositeSubscription!!.add(
-                dataSource.getArtObject(artObjectId!!)
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe { artObjectUi ->
-                            // TODO: Picasso.with(this).load(artObjectUi.thumbPicUrl).into(art_object_detail_image);
-                            // when server is ready
-                            Picasso.with(this).load(R.drawable.einstein).into(art_object_detail_image)
-                            art_object_detail_name.text = artObjectUi.name
-                            art_object_detail_author.text = artObjectUi.author.name
-                            art_object_detail_description.text = artObjectUi.description
-                        })
+    private fun startFetchingArtObject(): Subscription {
+        return dataSource.getArtObject(artObjectId!!)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { artObjectUi ->
+                    art_object_detail_name.text = artObjectUi.name
+                    art_object_detail_author.text = artObjectUi.author.name
+                    art_object_detail_description.text = artObjectUi.description
+
+                    val adapter = GalleryPagerAdapter(this, artObjectUi.picsUrls)
+                    art_object_detail_image_pager.adapter = adapter
+
+                    mapFragment?.getMapAsync {
+                        it.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(artObjectUi.lat, artObjectUi.lng), 14f))
+                        it.addArtObject(artObjectUi)
+                    }
+                }
     }
 
-    companion object {
-        val EXTRA_KEY_ART_OBJECT_DETAIL_ID = "EXTRA_KEY_ART_OBJECT_DETAIL_ID"
+    internal inner class GalleryPagerAdapter(val context: Context, val images: List<String>) : PagerAdapter() {
+        var inflater: LayoutInflater
+
+        init {
+            inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        }
+
+        override fun getCount(): Int {
+            return images.size
+        }
+
+        override fun isViewFromObject(view: View, anyObject: Any): Boolean {
+            return view === anyObject as LinearLayout
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val itemView = inflater.inflate(R.layout.art_object_gallery_item, container, false)
+            container.addView(itemView)
+
+            val thumbnailSize = (art_object_detail_image_pager.layoutParams as FrameLayout.LayoutParams).bottomMargin
+
+            val params = LinearLayout.LayoutParams(thumbnailSize, thumbnailSize)
+            params.setMargins(0, 0, 0, 0);
+
+            val thumbView = ImageView(context)
+            thumbView.scaleType = ImageView.ScaleType.CENTER_CROP
+            thumbView.layoutParams = params
+            thumbView.setOnClickListener {
+                art_object_detail_image_pager.currentItem = position
+            }
+            art_object_detail_image_picker.addView(thumbView)
+
+            Picasso.with(context).load(images[position]).into(itemView.image_preview)
+            Picasso.with(context).load(images[position]).into(thumbView)
+
+            return itemView
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, anyObject: Any) {
+            container.removeView(anyObject as LinearLayout)
+        }
     }
+
 }
---
+++ app/src/main/java/org/imozerov/streetartview/ui/extensions/MapExtensions.kt
@@ -0,0 +1,19 @@
+package org.imozerov.streetartview.ui.extensions
+
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import org.imozerov.streetartview.ui.model.ArtObjectUi
+
+/**
+ * Created by imozerov on 19.02.16.
+ */
+fun GoogleMap.addArtObject(artObject: ArtObjectUi) {
+    addMarker(MarkerOptions().position(LatLng(artObject.lat, artObject.lng))
+            .title(artObject.author.name)
+            .snippet(artObject.name)
+            .icon(BitmapDescriptorFactory.defaultMarker(
+                    BitmapDescriptorFactory.HUE_AZURE)))
+}
