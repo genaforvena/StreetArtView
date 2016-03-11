1af5cf424589eb644caa76cc071a8a8504890ff5
Sergei Novikov <Sergei.Novikov@ascom.com>
2016-03-11 12:39:01 +0300

    Local image cache
    Change-Id: I1670659f45a579f5a5a7650751e25657b1ffff2d

--- app/src/main/java/org/imozerov/streetartview/ui/extensions/UiExtensions.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/extensions/UiExtensions.kt
@@ -1,6 +1,9 @@
 package org.imozerov.streetartview.ui.extensions
 
+import android.util.Log
 import android.widget.ImageView
+import com.squareup.picasso.Callback
+import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
 
 /**
@@ -8,6 +11,29 @@ import com.squareup.picasso.Picasso
  */
 fun ImageView.loadImage(imagePath: String) {
     if (imagePath.isNotBlank()) {
-        Picasso.with(context).load(imagePath).into(this)
+        Picasso.with(context)
+                .load(imagePath)
+                .networkPolicy(NetworkPolicy.OFFLINE)
+                .into(this, object : Callback {
+                    override fun onSuccess() {
+
+                    }
+
+                    override fun onError() {
+                        // Try again online if cache failed
+                        Picasso.with(context)
+                                .load(imagePath)
+                                //                                .error(R.drawable.header)
+                                .into(this@loadImage, object : Callback {
+                                    override fun onSuccess() {
+
+                                    }
+
+                                    override fun onError() {
+                                        Log.w("Picasso", "Could not fetch image")
+                                    }
+                                })
+                    }
+                })
     }
 }
