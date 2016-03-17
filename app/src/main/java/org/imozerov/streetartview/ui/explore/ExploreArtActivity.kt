ec7cab08482e5c9ecce4e842a1fe827f83e834c3
Ilya Mozerov <imozerov@mera.ru>
2016-03-17 11:04:21 +0300

    Fixed NPE.

--- app/src/main/java/org/imozerov/streetartview/ui/explore/ExploreArtActivity.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/explore/ExploreArtActivity.kt
@@ -73,7 +73,7 @@ class ExploreArtActivity : AppCompatActivity(), ArtObjectDetailOpener {
     }
 
     override fun onBackPressed() {
-        if (getMapFragmentIfCurrentOrNull()?.onBackPressed()!!) {
+        if (getMapFragmentIfCurrentOrNull()?.onBackPressed() == true) {
             return
         }
 
