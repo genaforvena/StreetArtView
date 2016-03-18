be4817c77eba8acf51e062168266d895ddc6f68d
Ilya Mozerov <imozerov@mera.ru>
2016-03-18 09:45:23 +0300

    Removed commented code

--- app/src/main/java/org/imozerov/streetartview/ui/extensions/UiExtensions.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/extensions/UiExtensions.kt
@@ -11,29 +11,7 @@ val TAG = "UiExtensions"
 
 fun ImageView.loadImage(imagePath: String) {
     if (imagePath.isNotBlank()) {
-        Glide.with(context)
-                .load(imagePath)
-                .into(this)
-        //                .networkPolicy(NetworkPolicy.OFFLINE)
-        //                .into(this, object : Callback {
-        //                    override fun onSuccess() {
-        //
-        //                    }
-        //
-        //                    override fun onError() {
-        //                        // Try again online if cache failed
-        //                        Picasso.with(context)
-        //                                .load(imagePath)
-        //                                .into(this@loadImage, object : Callback {
-        //                                    override fun onSuccess() {
-        //                                    }
-        //
-        //                                    override fun onError() {
-        //                                        Log.w(TAG, "Could not fetch image")
-        //                                    }
-        //                                })
-        //                    }
-        //                })
+        Glide.with(context).load(imagePath).into(this)
     } else {
         Glide.with(context).load(R.drawable.einstein).into(this)
     }
