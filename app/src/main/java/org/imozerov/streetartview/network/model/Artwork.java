87477477a79f4507e373772dc2d427eda673ae05
Ilya Mozerov <imozerov@mera.ru>
2016-04-18 14:30:27 +0300

    Increased schema version in Realm to avoid duplicates in clients data.

--- app/src/main/java/org/imozerov/streetartview/network/model/Artwork.java
+++ app/src/main/java/org/imozerov/streetartview/network/model/Artwork.java
@@ -190,6 +190,8 @@ public class Artwork {
                 ", status=" + status +
                 ", artists=" + artists +
                 ", photos=" + photos +
+                ", updatedAt=" + updatedAt +
+                ", createdAt=" + createdAt +
                 '}';
     }
 }
