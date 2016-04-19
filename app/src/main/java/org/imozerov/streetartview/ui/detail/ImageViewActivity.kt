26573ea2ae62846404d4054f429f51460d98602d
Ilya Mozerov <imozerov@mera.ru>
2016-04-19 13:25:22 +0300

    Replace image view activity xml with anko.

--- app/src/main/java/org/imozerov/streetartview/ui/detail/ImageViewActivity.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/detail/ImageViewActivity.kt
@@ -2,10 +2,13 @@ package org.imozerov.streetartview.ui.detail
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_image_view.*
-import org.imozerov.streetartview.R
+import android.view.Gravity
 import org.imozerov.streetartview.StreetArtViewApp
 import org.imozerov.streetartview.storage.IDataSource
+import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.linearLayout
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.support.v4.viewPager
 import javax.inject.Inject
 
 class ImageViewActivity : AppCompatActivity() {
@@ -15,16 +18,23 @@ class ImageViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_image_view)
-
         (application as StreetArtViewApp).appComponent.inject(this)
 
         val artObjectId = intent.getStringExtra(DetailArtObjectActivity.EXTRA_KEY_ART_OBJECT_DETAIL_ID)
         val artObjectUi = dataSource.getArtObject(artObjectId)
-
-        image_viewpager.adapter = GalleryWithZoomPagerAdapter(this, artObjectUi.picsUrls)
         val imageChosen = intent.getIntExtra(DetailArtObjectActivity.EXTRA_IMAGE_CHOSEN_IN_DETAILS, 0)
-        image_viewpager.currentItem = imageChosen
+
+        linearLayout {
+            backgroundResource = android.R.color.black
+
+            viewPager {
+                lparams(width = matchParent, height = matchParent) {
+                    gravity = Gravity.CENTER_VERTICAL
+                    adapter = GalleryWithZoomPagerAdapter(this@ImageViewActivity, artObjectUi.picsUrls)
+                    currentItem = imageChosen
+                }
+            }
+        }
     }
 
     companion object {
