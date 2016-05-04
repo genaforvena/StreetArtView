88158e2d307e1064858d7362ecc58793a9e7ce73
Ilya Mozerov <imozerov@mera.ru>
2016-05-04 21:59:06 +0300

    Added swipe to image view activity. It'll work for now as we do not have any objects with multiple images in it.

--- app/src/main/java/org/imozerov/streetartview/ui/detail/ImageViewActivity.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/detail/ImageViewActivity.kt
@@ -1,17 +1,20 @@
 package org.imozerov.streetartview.ui.detail
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.ViewManager
+import com.liuguangqiang.swipeback.SwipeBackActivity
+import com.liuguangqiang.swipeback.SwipeBackLayout
 import org.imozerov.streetartview.StreetArtViewApp
 import org.imozerov.streetartview.storage.IDataSource
 import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.custom.ankoView
 import org.jetbrains.anko.linearLayout
 import org.jetbrains.anko.matchParent
 import org.jetbrains.anko.support.v4.viewPager
 import javax.inject.Inject
 
-class ImageViewActivity : AppCompatActivity() {
+class ImageViewActivity : SwipeBackActivity() {
 
     @Inject
     lateinit var dataSource: IDataSource
@@ -25,15 +28,16 @@ class ImageViewActivity : AppCompatActivity() {
         val imageChosen = intent.getIntExtra(DetailArtObjectActivity.EXTRA_IMAGE_CHOSEN_IN_DETAILS, 0)
 
         linearLayout {
-            backgroundResource = android.R.color.black
-
-            viewPager {
-                lparams(width = matchParent, height = matchParent) {
-                    gravity = Gravity.CENTER_VERTICAL
-                    adapter = GalleryWithZoomPagerAdapter(this@ImageViewActivity, artObjectUi.picsUrls)
-                    currentItem = imageChosen
+            swipeBackLayout {
+                viewPager {
+                    backgroundResource = android.R.color.black
+                    lparams(width = matchParent, height = matchParent) {
+                        gravity = Gravity.CENTER_VERTICAL
+                        adapter = GalleryWithZoomPagerAdapter(this@ImageViewActivity, artObjectUi.picsUrls)
+                        currentItem = imageChosen
+                    }
                 }
-            }
+            }.setDragEdge(SwipeBackLayout.DragEdge.LEFT);
         }
     }
 
@@ -41,3 +45,7 @@ class ImageViewActivity : AppCompatActivity() {
         val EXTRA_IMAGE_CHOSEN_IN_VIEWPAGER = "EXTRA_IMAGE_CHOSEN_IN_VIEWPAGER"
     }
 }
+
+inline fun ViewManager.swipeBackLayout(init: SwipeBackLayout.() -> Unit) = ankoView({ SwipeBackLayout(it) }, init)
+
+
