b9c3dc54b41362bd6856275f6610500bc760446d
Ilya Mozerov <imozerov@mera.ru>
2016-05-10 20:41:41 +0300

    Can pick image from gallery.

--- app/src/main/java/org/imozerov/streetartview/ui/add/PickImageActivity.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/add/PickImageActivity.kt
@@ -16,11 +16,8 @@ import kotlinx.android.synthetic.main.activity_add_art_object.*
 import kotlinx.android.synthetic.main.art_object_in_list.view.*
 import org.imozerov.streetartview.R
 import org.imozerov.streetartview.ui.custom.CursorRecyclerViewAdapter
-import org.imozerov.streetartview.ui.model.ArtObjectUi
-import java.util.*
 
-class PickImageActivity : AppCompatActivity() {
-
+class PickImageActivity : AppCompatActivity(), OnImagePickListener {
     private val adapter by lazy { SdCardImagesAdapter(this, cursor!!) }
 
     private var cursor: Cursor? = null
@@ -39,27 +36,47 @@ class PickImageActivity : AppCompatActivity() {
         sd_card_images.adapter = adapter
     }
 
-    class SdCardImagesAdapter(context: Context, cursor: Cursor) : CursorRecyclerViewAdapter<SdCardImagesAdapter.ViewHolder>(context, cursor) {
+    override fun onImagePicked(imageUri: Uri?) {
+        image_picked.setImageURI(imageUri)
+    }
+
+    class SdCardImagesAdapter(private val context: Context, cursor: Cursor) : CursorRecyclerViewAdapter<SdCardImagesAdapter.ViewHolder>(context, cursor) {
         val imageIdCursorIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID)
 
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        class ViewHolder(context: Context, view: View) : RecyclerView.ViewHolder(view) {
             var imageView: ImageView
+            var imageUri: Uri? = null
 
             init {
+                val imagePickListener: OnImagePickListener
+                try {
+                    imagePickListener = context as OnImagePickListener
+                } catch (cce: ClassCastException) {
+                    throw RuntimeException("$context must implement ArtObjectDetailOpener interface")
+                }
+
                 imageView = view.art_object_in_list_image
+                imageView.setOnClickListener { imagePickListener.onImagePicked(imageUri) }
             }
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             val itemView = LayoutInflater.from(parent.context).inflate(R.layout.art_object_in_list, parent, false)
-            val vh = ViewHolder(itemView)
+            val vh = ViewHolder(context, itemView)
             return vh
         }
 
         override fun onBindViewHolder(viewHolder: ViewHolder, cursor: Cursor) {
             val imageID = cursor.getInt(imageIdCursorIndex)
-            viewHolder.imageView.setImageURI(Uri.withAppendedPath(
-                    MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + imageID))
+            val uri = Uri.withAppendedPath(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + imageID)
+            viewHolder.imageView.setImageURI(uri)
+            viewHolder.imageUri = uri
         }
+
+
     }
+}
+
+interface OnImagePickListener {
+    fun onImagePicked(imageUri: Uri?)
 }
