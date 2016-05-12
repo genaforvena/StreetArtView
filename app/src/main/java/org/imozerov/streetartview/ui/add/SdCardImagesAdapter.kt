807a210e2ce812966e80f98aa1d8572e3495c66c
Sergei Novikov <sanovikov71@gmail.com>
2016-05-12 17:58:36 +0300

    pick image activity
    Change-Id: I660f58dac9a0bcca730eacc3564d97b7ef6ccf4f

--- app/src/main/java/org/imozerov/streetartview/ui/add/SdCardImagesAdapter.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/add/SdCardImagesAdapter.kt
@@ -1,51 +1,77 @@
 package org.imozerov.streetartview.ui.add
 
 import android.content.Context
-import android.database.Cursor
 import android.net.Uri
-import android.provider.MediaStore
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import kotlinx.android.synthetic.main.art_object_in_list.view.*
+import com.bumptech.glide.Glide
 import org.imozerov.streetartview.R
-import org.imozerov.streetartview.ui.custom.CursorRecyclerViewAdapter
+import java.util.*
 
-/**
- * Created by imozerov on 11.05.16.
- */
-class SdCardImagesAdapter(private val context: Context, cursor: Cursor) : CursorRecyclerViewAdapter<SdCardImagesAdapter.ViewHolder>(context, cursor) {
-    val imageIdCursorIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID)
+class SdCardImagesAdapter(private val mContext: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var mData: List<PictureItem> = ArrayList<PictureItem>()
 
-    class ViewHolder(context: Context, view: View) : RecyclerView.ViewHolder(view) {
-        var imageView: ImageView
-        var imageId: Int? = null
+    val TAG = "SdCardImagesAdapter"
+
+    fun updateDataset(data: List<PictureItem>) {
+        Log.i(TAG, "updateDataset")
+        mData = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.art_object_in_list, parent, false)
+        return ViewHolder(mContext, view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val typedHolder = holder as ViewHolder
+
+        val pictureItem = mData[position]
+
+        typedHolder.mItemId = pictureItem.id!!
+
+        Glide.with(mContext).load(Uri.withAppendedPath(PickImageActivity.IMAGES_PATH,
+                pictureItem.id.toString()))
+                .thumbnail(0.1f)
+                .into(typedHolder.mImage)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemCount(): Int {
+        return mData.size
+    }
+
+    internal class ViewHolder(context: Context, view: View) : RecyclerView.ViewHolder(view) {
+
+        var mItemId: Int = 0
+        var mImage: ImageView
 
         init {
+            mImage = view.findViewById(R.id.art_object_in_list_image) as ImageView
+
             val imagePickListener: OnImagePickListener
             try {
                 imagePickListener = context as OnImagePickListener
             } catch (cce: ClassCastException) {
-                throw RuntimeException("$context must implement ArtObjectDetailOpener interface")
+                throw RuntimeException("$context must implement OnImagePickListener interface")
             }
 
-            imageView = view.art_object_in_list_image
-            imageView.setOnClickListener { imagePickListener.onImagePicked(imageId) }
+            view.setOnClickListener { imagePickListener.onImagePicked(mItemId) }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.art_object_in_list, parent, false)
-        val vh = ViewHolder(context, itemView)
-        return vh
-    }
-
-    override fun onBindViewHolder(viewHolder: ViewHolder, cursor: Cursor) {
-        val imageId = cursor.getInt(imageIdCursorIndex)
-        val uri = Uri.withAppendedPath(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, imageId.toString())
-        viewHolder.imageView.setImageURI(uri)
-        viewHolder.imageId = imageId
-    }
 }
+
+interface OnImagePickListener {
+    fun onImagePicked(imageId: Int)
+}
+
+
