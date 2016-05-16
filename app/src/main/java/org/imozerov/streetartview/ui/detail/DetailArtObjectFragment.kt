a6aa0b538f91a689a44efa55733ac9ca8231a3be
luckyace <bulatowf@yandex.ru>
2016-05-16 16:37:44 +0300

    bottomsheet draft version

--- app/src/main/java/org/imozerov/streetartview/ui/detail/DetailArtObjectFragment.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/detail/DetailArtObjectFragment.kt
@@ -6,20 +6,25 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.support.v4.app.Fragment
+import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.RelativeLayout
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
+import com.flipboard.bottomsheet.BaseViewTransformer
+import com.flipboard.bottomsheet.BottomSheetLayout
+import com.flipboard.bottomsheet.ViewTransformer
+import com.flipboard.bottomsheet.commons.BottomSheetFragment
 import com.google.android.gms.analytics.Tracker
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
 import com.jakewharton.rxbinding.view.RxView
-import com.liuguangqiang.swipeback.SwipeBackLayout
 import kotlinx.android.synthetic.main.fragment_detail.*
 import org.imozerov.streetartview.R
 import org.imozerov.streetartview.R.layout.fragment_detail
@@ -41,7 +46,7 @@ import javax.inject.Inject
  * Created by ibulatov on 10.05.2016.
  */
 
-class DetailArtObjectFragment : Fragment() {
+class DetailArtObjectFragment : BottomSheetFragment() {
 
     val PICK_IMAGE_REQUEST = 1
 
@@ -56,6 +61,12 @@ class DetailArtObjectFragment : Fragment() {
 
     val compositeSubscription: CompositeSubscription = CompositeSubscription()
 
+    private val mDetailImageMinHeight by lazy {activity.resources.getDimension(R.dimen.detail_image_min_height)}
+    private val mDetailImageMaxHeight by lazy {activity.resources.getDimension(R.dimen.detail_image_max_height)}
+
+    private var mDetailImageMinWidth: Float = 0f
+    private var mDetailImageMaxWidth: Float = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity.applicationContext as StreetArtViewApp).appComponent!!.inject(this)
@@ -68,7 +79,7 @@ class DetailArtObjectFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        swipeBackLayout.setDragEdge(SwipeBackLayout.DragEdge.LEFT)
+//        swipeBackLayout.setDragEdge(SwipeBackLayout.DragEdge.LEFT)
         tracker.sendScreen("DetailArtObjectFragment: ${artObjectUi.name}")
 
         bindViews()
@@ -102,6 +113,15 @@ class DetailArtObjectFragment : Fragment() {
     }
 
     private fun bindViews() {
+
+        collapsing_toolbar.viewTreeObserver.addOnGlobalLayoutListener(object: ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                mDetailImageMaxWidth = collapsing_toolbar.width.toFloat()
+                mDetailImageMinWidth = mDetailImageMaxWidth!! * mDetailImageMinHeight / mDetailImageMaxHeight
+                collapsing_toolbar.viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+        })
+
         art_object_detail_name.text = "\"${artObjectUi.name}\""
         art_object_detail_author.text = artObjectUi.authorsNames()
         if (artObjectUi.description.isBlank()) {
@@ -204,6 +224,32 @@ class DetailArtObjectFragment : Fragment() {
             detail_set_favourite_button.setImageDrawable(activity.getDrawableSafely(R.drawable.ic_favorite_border_black_24dp))
         }
     }
+
+    override fun getViewTransformer(): ViewTransformer {
+        return object : BaseViewTransformer() {
+            override fun transformView(translation: Float, maxTranslation: Float, peekedTranslation: Float, parent: BottomSheetLayout, view: View) {
+
+                val progress = (translation - peekedTranslation) / (maxTranslation - peekedTranslation)
+
+                art_object_collapsing_content.layoutParams.height = (mDetailImageMinHeight + progress * (mDetailImageMaxHeight - mDetailImageMinHeight)).toInt()
+                collapsing_toolbar.layoutParams.height = (mDetailImageMinHeight + progress * (mDetailImageMaxHeight - mDetailImageMinHeight)).toInt()
+
+                val lp: CollapsingToolbarLayout.LayoutParams = art_object_collapsing_content.layoutParams as CollapsingToolbarLayout.LayoutParams
+
+                if(translation == maxTranslation) {
+                    lp.width = RelativeLayout.LayoutParams.MATCH_PARENT
+                    lp.height = mDetailImageMaxHeight.toInt()
+                } else {
+                    lp.width = (mDetailImageMinWidth + (mDetailImageMaxWidth - mDetailImageMinWidth) * progress).toInt();
+                    lp.height = (mDetailImageMinHeight + (mDetailImageMaxHeight - mDetailImageMinHeight) * progress).toInt();
+                }
+
+                art_object_collapsing_content.requestLayout()
+                collapsing_toolbar.requestLayout()
+            }
+        }
+    }
+
 
     companion object {
 
