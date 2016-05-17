f797202efc338fe0f85955a179b9f9728c489741
Ilya Mozerov <imozerov@mera.ru>
2016-05-17 13:06:12 +0300

    Minor navigation bar improvements.

--- app/src/main/java/org/imozerov/streetartview/ui/explore/ExploreArtActivity.kt
+++ app/src/main/java/org/imozerov/streetartview/ui/explore/ExploreArtActivity.kt
@@ -206,25 +206,3 @@ class ExploreArtActivity : AppCompatActivity(), ArtObjectDetailOpener {
         return null
     }
 }
-
-private class Adapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-    private val mFragments = ArrayList<Fragment>()
-    private val mFragmentTitles = ArrayList<String>()
-
-    fun addFragment(fragment: Fragment, title: String) {
-        mFragments.add(fragment)
-        mFragmentTitles.add(title)
-    }
-
-    override fun getItem(position: Int): Fragment {
-        return mFragments[position]
-    }
-
-    override fun getCount(): Int {
-        return mFragments.size
-    }
-
-    override fun getPageTitle(position: Int): CharSequence {
-        return ""
-    }
-}
